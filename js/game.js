fa4a18eb6d49fff60116d223fa9e756775d148a9
Maryam <Nadiri.maryam@gmail.com>
2024-09-22 23:44:23 +0330

    2

--- js/game.js
+++ js/game.js
@@ -1,1 +1,28 @@
-console.log("game start")
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const URL =
+"https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple"
+let formattedData= null;
+ const formatData = questionData => {
+   const result = questionData.map(item => {
+    const questionObject= {question: item.question};
+    const answers = [...item.incorrect_answers];
+    const correctAnswerIndex = Math.floor(Math.random() * 4)
+    console.log (answers,correctAnswerIndex)
+   });
+ };
+
+const fetchData = async () => {
+    const response = await fetch(URL);
+    const json = await response.json();
+    // formattedData= json;
+    formatData(json.results);
+    start();
+};
+
+const start =() => {
+    loader.style.display = "none";
+    container.style.display = "block";
+};
+window.addEventListener("load", fetchData);
